917562003a40b9db724b61c10a178051edf63a1d
ColonelKassad <Alexander.Haritonov@gmail.com>
2012-11-20 02:55:40 +0100

    return statements corrected

--- Scheduler/jni/com_smartroute_util_SchedulerApi.cpp
+++ Scheduler/jni/com_smartroute_util_SchedulerApi.cpp
@@ -3,12 +3,14 @@
 
 JNIEXPORT jstring JNICALL Java_com_smartroute_util_SchedulerApi_fnTryScheduleFavorable (JNIEnv *env, jobject obj, jintArray orderIDs, jint maxSuggestionsPerOrder)
 {
-	return str;
+	jstring ret = (*env)->NewStringUTF(env, "<suggestions></suggestions>");
+  return ret;
 }
 
 JNIEXPORT jstring JNICALL Java_com_smartroute_util_SchedulerApi_fnBestAvailable (JNIEnv *env, jobject obj, jintArray orderIDs, jint maxSuggestionsPerOrder)
 {
-  return str;
+  jstring ret = (*env)->NewStringUTF(env, "<suggestions></suggestions>");
+  return ret;
 }
 
 JNIEXPORT jint JNICALL Java_com_smartroute_util_SchedulerApi_fnEstimatePrice(JNIEnv *env, jobject obj, jstring orderXML)
