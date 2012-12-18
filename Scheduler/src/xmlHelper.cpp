858ad15d5491864963e3e512094a91b227a8d848
ColonelKassad <Alexander.Haritonov@gmail.com>
2012-12-18 17:18:59 +0100

    erster xml parsing test zum laufen (und kompilieren) gekriegt. (Zusatz: noch einige Dateien)

--- Scheduler/src/xmlHelper.cpp
+++ Scheduler/src/xmlHelper.cpp
@@ -1,5 +1,7 @@
 #include "xmlHelper.h"
 
+#include "shipmentStation.h"
+
 namespace Scheduler
 {
   std::auto_ptr<CContract> CXmlHelper::ReadContract(const std::string& xmlString)
@@ -7,4 +9,34 @@ namespace Scheduler
     // TODO
     return std::auto_ptr<CContract>(new CContract);
   }
+
+  bool CXmlHelper::ParseStationLoadingKind(const TiXmlElement* xmlElement, CShipmentStation::EKind& eKind)
+  {
+    eKind = CShipmentStation::KDriveby;
+
+    const char* cstr = xmlElement->GetText();
+    if (!cstr)
+    {
+      return false;
+    }
+
+    if (strncmp(cstr, "LOAD", 20) == 0)
+    {
+      eKind = CShipmentStation::KLoad;
+      return true;
+    }
+    else if (strncmp(cstr, "UNLOAD", 20) == 0)
+    {
+      eKind = CShipmentStation::KUnload;
+      return true;
+    }
+    else if (strncmp(cstr, "DRIVE_BY", 20) == 0)
+    {
+      eKind = CShipmentStation::KDriveby;
+      return true;
+    }
+
+    return false;
+  }
+
 }
--- Scheduler/src/xmlHelper.h
+++ Scheduler/src/xmlHelper.h
@@ -6,16 +6,31 @@
 
 #include <auto_ptr.h>
 
+#define TIXML_USE_STL
 #include "tinyxml.h"
 
 #include "contract.h"
 
 namespace Scheduler
 {
+  namespace Test
+  {
+    class CXmlTestFixture;
+  }
+
   class CXmlHelper
   {
   public:
     std::auto_ptr<CContract> ReadContract(const std::string& xmlString);
+
+  protected:
+    void LoadXml(const std::string& xmlStr) { _currentDoc.Parse(xmlStr.c_str(), 0, TIXML_ENCODING_UTF8); }
+
+    bool ParseStationLoadingKind(const TiXmlElement* xmlElement, CShipmentStation::EKind& eKind);
+
+    TiXmlDocument _currentDoc;
+
+    friend class Test::CXmlTestFixture;
   };
 }
 
